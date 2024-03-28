29ffb374b22f673e052bc78e95280d411cfed6b6
CalenFeaElen <calenfeaelen@gmail.com>
2024-03-28 13:27:37 -0700

    Added category and option to filter by category

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Loader } from "react-feather";
 import "./App.css";
-import Quotes from "./components/Quotes";
+import Quotes from "./components/quotes/Quotes";
 
 function App() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [category, setCategory] = useState("All");
+
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
+  const categories = [
+    "All",
+    "Leadership",
+    "Empathy",
+    "Motivation",
+    "Learning",
+    "Success",
+    "Empowerment",
+  ];
 
   const fetchQuotes = async () => {
     try {
@@ -28,12 +39,27 @@ function App() {
   }, []);
   console.log(quotes);
 
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const filteredQuotes =
+    category !== "All"
+      ? quotes.filter((quote) => quote.categories.includes(category))
+      : quotes;
+
   return (
     <div className="App">
       <Header />
       <main>
         {loading === true && <Loader />}
-        <Quotes quotes={quotes} />
+        <Quotes
+          quotes={quotes}
+          categories={categories}
+          category={category}
+          handleCategoryChange={handleCategoryChange}
+          filteredQuotes={filteredQuotes}
+        />
       </main>
       <Footer />
     </div>
---
+++ src/components/quotes/Quotes.js
@@ -0,0 +1,31 @@
+import React from "react";
+import QuoteCard from "./QuoteCard";
+import CategoryForm from "./CategoryForm";
+
+const Quotes = ({
+  filteredQuotes,
+  categories,
+  category,
+  handleCategoryChange,
+}) => {
+  return (
+    <section className="all-quotes">
+      <div className="quotes wrapper">
+        <div class="category-header">
+          <p>Browse through your collection of quotes</p>
+          <CategoryForm
+            categories={categories}
+            category={category}
+            handleCategoryChange={handleCategoryChange}
+          />
+        </div>
+        {filteredQuotes.map((quote) => (
+          <QuoteCard key={quote.id} quote={quote} />
+        ))}
+        ;
+      </div>
+    </section>
+  );
+};
+
+export default Quotes;
